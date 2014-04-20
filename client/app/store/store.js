22b75f0ba464d17a497d25deb19c9269e18ef214
Joseph DeWilde <jzdewilde@alaska.edu>
2014-04-19 20:31:03 -0800

    update

--- client/app/store/store.js
+++ client/app/store/store.js
@@ -2,7 +2,7 @@ var store = angular.module('store', []);
 
 store.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/store', {
-    templateUrl:'app/store/store.tpl.html',
+    templateUrl:'app/store/store.tpl.ejs',
     resolve: store.resolve
   });
 }]);
