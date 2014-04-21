fc4838cd9a8578c974ad4e0a57754d1d4846ff2e
Joseph DeWilde <jzdewilde@alaska.edu>
2014-04-21 01:48:25 -0800

    Added routes for /createItem and /getItems

--- server/app/models/store.js
+++ server/app/models/store.js
@@ -1,35 +1,21 @@
-// app/models/user.js
+// app/models/store.js
 // load the things we need
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
 
 // define the schema for our user model
 var storeSchema = mongoose.Schema({
 
-  store                 : {
-    username            : String,
-    password            : String,
-    email               : String,
-    firstName           : String,
-    lastName            : String,
-    storeName           : String,
-    points              : Number,
-    coins               : Number
-   
-  },
+    name     : String, // TODO: Probably should make sure names are unique
+    coins    : Number,
+    items    : [{
+                 name : String,
+                 description : String,
+                 cost : Number//,
+                 //expires: Date
+                }]
 });
 
 // methods =====================
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.local.password);
-};
-
 // create the model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Store', storeSchema);
 
--- server/app/routes/routes.js
+++ server/app/routes/routes.js
@@ -1,5 +1,6 @@
 var Post = require('../models/post');
 var User = require('../models/user');
+var Store = require('../models/store');
 
 module.exports = function(app) {
 
@@ -94,6 +95,155 @@ module.exports = function(app) {
 	    });
 	 });
 
+	app.post('/inventory', function(req, res, next) {
+		// Check to see if user exists
+	    User.findOne({ 'local.email' : req.body.email }, function(err, user) {
+	      // if there are any errors, return the error before anything else
+	      if (err)
+	      {
+	        console.log("Error in finding user");
+	        return next(err);
+	      }
+
+	      // if no user is found, return the message
+	      if(!user)
+	      {
+	        console.log("No user is found");
+	        return res.json({err: null, store: false, message: "User does not exist."});
+	      }
+
+	      // if user does not have a store then they have no inventory
+	      if(!user.local.storeName)
+	      {
+	      	console.log("User does not have a store");
+	      	return res.json({err: null, store: false, message: "User does not have a store"});
+	      }
+
+		  console.log("User has a store");
+		  Store.findOne({'_id' : user.local.storeName}, function(err, store) {
+		  	if(err)
+		  	{
+		  		console.log("Error in finding store");
+		  		return next(err);
+		  	}
+		  	if(!store)
+		  	{
+		  		console.log("User's store is undefined (no data)");
+		  		return res.json({err: null, store: false, message: "No store found"});
+		  	}
+		  	console.log("Found store");
+		  	return res.json({err: null, store: store.items});
+		  })
+	    });
+	 });
+
+app.post('/createItem', function(req, res, next) {
+		// Check to see if user exists
+	    User.findOne({ 'local.email' : req.body.email }, function(err, user) {
+	      // if there are any errors, return the error before anything else
+	      if (err)
+	      {
+	        console.log("Error in finding user");
+	        return next(err);
+	      }
+
+	      // if no user is found, return the message
+	      if(!user)
+	      {
+	        console.log("No user is found");
+	        return res.json({err: null, items: false, message: "User does not exist."});
+	      }
+
+	      // if user does not have a store then they can't create items
+	      if(!user.local.storeName)
+	      {
+	      	console.log("User does not have a store");
+	      	return res.json({err: null, items: false, message: "User does not have a store"});
+	      }
+
+		  console.log("User has a store");
+		  Store.find(user.local.storeName, function(err, store) {
+		  	if(err)
+		  	{
+		  		console.log("Error in finding store");
+		  		return next(err);
+		  	}
+		  	if(!store)
+		  	{
+		  		console.log("User's store is undefined (no data)");
+		  		return res.json({err: null, items: false, message: "No store found"});
+		  	}
+		  	console.log("Found store");
+		  	// TODO: Probably should make sure item is valid eg name, desc are nonempty, coins is positive, date is valid
+		  	Store.update({name: user.local.storeName},{$push:{items:req.body.item}},{safe: true, upsert:true}, function(err, model) {
+		  		if(err){
+		  			console.log(err);
+		  			return res.json({err:err});
+		  		}
+		  		else{
+		  			if(!model){
+		  				console.log("Failed to add item");
+		  				return res.json({err:null, items: model.items});
+		  			}
+		  			console.log("Succesfully added item");
+		  			return res.json({err:null, items:model.items})
+		  		}
+		  	});
+		  })
+	    });
+	 });
+
+app.post('/getItems', function(req, res, next) {
+		// Check to see if user exists
+	    User.findOne({ 'local.email' : req.body.email }, function(err, user) {
+	      // if there are any errors, return the error before anything else
+	      if (err)
+	      {
+	        console.log("Error in finding user");
+	        return next(err);
+	      }
+
+	      // if no user is found, return the message
+	      if(!user)
+	      {
+	        console.log("No user is found");
+	        return res.json({err: null, items: false, message: "User does not exist."});
+	      }
+
+	      // if user does not have a store then they can't get items
+	      if(!user.local.storeName)
+	      {
+	      	console.log("User does not have a store");
+	      	return res.json({err: null, items: false, message: "User does not have a store"});
+	      }
+
+		  console.log("User has a store");
+		  Store.find(user.local.storeName, function(err, store) {
+		  	if(err)
+		  	{
+		  		console.log("Error in finding store");
+		  		return next(err);
+		  	}
+		  	if(!store)
+		  	{
+		  		console.log("User's store is undefined (no data)");
+		  		return res.json({err: null, items: false, message: "No store found"});
+		  	}
+		  	console.log("Found store");
+		  	// TODO: Probably should make sure item is valid eg name, desc are nonempty, coins is positive, date is valid
+		  	Store.findOne({name: user.local.storeName}, function(err, model) {
+		  		if(err){
+		  			console.log(err);
+		  			return res.json({err:err});
+		  		}
+		  		else{
+		  			console.log("Succesfully retrieved item");
+		  			return res.json({err:null, items:model.items})
+		  		}
+		  	});
+		  })
+	    });
+	 });
 }
 
 
