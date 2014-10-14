638fbe3a2162e35f13976291b211ab19c0cb50fb
Joseph DeWilde <jzdewilde@alaska.edu>
2014-10-14 11:37:29 -0800

    Replaced URL that connects to our local db to the URL that connects to the db at MongoLabs.com

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,6 @@
 // config/database.js
 
 module.exports = {
-  url : 'mongodb://localhost/passtor'
+  //url : 'mongodb://localhost/passtor'
+  url: "mongodb://Devauld:30YIaX9ofBWw@ds041380.mongolab.com:41380/passtor"
 };
